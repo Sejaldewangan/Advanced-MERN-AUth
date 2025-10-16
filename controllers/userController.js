9c7a535b9b7d2f6d18a625ec97d48e03b88aadba
sejaldewangan <shubham.dewangan.3440@gmail.com>
2025-10-16 18:23:03 +0530

    using zod

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import tryCatch from '../middleware/tryCatch.js'
 import sanitize from "mongo-sanitize";
+import { registerSchema } from '../config/zod.js';
 const registerUser = tryCatch(async (req,res) => {
-   sanitize (req.body) = {name,email,password}
- res.json({name,email,password})
+    const sanitizedBody = sanitize(req.body)
 
+const validation = registerSchema.safeParse(sanitizedBody)
+ if (!validation.success){
+    res.status(400).json("validation error")
+ } 
+
+  const {name,email,password} = validation.data
+  res.json({name ,email,password})
 })
 
 export {registerUser}
